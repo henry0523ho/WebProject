acb2744dbd29a90b899f07d6b0132d30a22d3182
henry0523ho <80961659+henry0523ho@users.noreply.github.com>
2022-01-08 06:44:11 +0800

    Add files via upload

--- script/shoot.js
+++ script/shoot.js
@@ -1,41 +1,37 @@
-var ScreenWidth = window.innerWidth;
-var ScreenHeight = window.innerHeight;
 var score = 0;
 
 $(document).ready(function() {
-    startGame();
-    $('#target').click(function() {
-        ++score;
-        $('#UI').html(("<h1>" + score + "/20</h1>"))
-        $('#target').css('animation-name', 'hit');
+    $('#TP3').mousedown(function() {
+        update();
+        $('#T3').css('animation', 'collapse 1s forwards');
         setTimeout(function() {
-            let x = Math.floor(Math.random() * ScreenWidth);
-            let y = Math.floor(Math.random() * ScreenHeight);
-            console.log(x, y);
-            while (y > ScreenHeight - 100) {
-                y = Math.floor(Math.random() * ScreenHeight);
-            }
-            while (x > ScreenWidth - 100) {
-                x = Math.floor(Math.random() * ScreenWidth);
-            }
-            y -= ScreenHeight / 10;
-            $('#target').css('top', y);
-            $('#target').css('left', x);
-            $('#target').css('animation-name', 'none');
-        }, 1001);
-        if (score == 3) {
-            $('#Win').css('display', 'block');
-        }
-    });
+            $('#T3').css('animation', 'recover 1s forwards');
+        }, 1000);
 
+    });
+    $('#TP2').mousedown(function() {
+        update();
+        $('#T2').css('animation', 'collapse 1s forwards');
+        setTimeout(function() {
+            $('#T2').css('animation', 'recover 1s forwards');
+        }, 2000);
+
+    });
+    $('#TP1').mousedown(function() {
+        update();
+        $('#T1').css('animation', 'collapse 1s forwards');
+        setTimeout(function() {
+            $('#T1').css('animation', 'recover 1s forwards');
+        }, 2000);
+    });
 
 });
 
-function startGame() {
-    let x = Math.floor(Math.random() * ScreenWidth);
-    let y = Math.floor(Math.random() * ScreenHeight);
-    y -= ScreenHeight / 10;
-    console.log(x, y);
-    $('#target').css('top', 0);
-    $('#target').css('left', 0);
+
+function update() {
+    ++score;
+    $('#UIText').html((score + '/20'));
+    if (score == 20) {
+        $('#Win').css('display', 'block');
+    }
 }
