2c1d9d646c1bcb5eb9e96cf0115c2c1e03ba32ee
henry0523ho <80961659+henry0523ho@users.noreply.github.com>
2022-01-10 14:11:44 +0800

    Add files via upload

--- script/shoot.js
+++ script/shoot.js
@@ -3,23 +3,21 @@ var score = 0;
 $(document).ready(function() {
     $('#TP3').mousedown(function() {
         update();
-        $('#T3').css('animation', 'collapse 1s forwards');
+        $('#T3').css('animation', 'collapse 0.5s forwards');
         setTimeout(function() {
             $('#T3').css('animation', 'recover 1s forwards');
-        }, 1000);
-
+        }, 2000);
     });
     $('#TP2').mousedown(function() {
         update();
-        $('#T2').css('animation', 'collapse 1s forwards');
+        $('#T2').css('animation', 'collapse 0.5s forwards');
         setTimeout(function() {
             $('#T2').css('animation', 'recover 1s forwards');
         }, 2000);
-
     });
     $('#TP1').mousedown(function() {
         update();
-        $('#T1').css('animation', 'collapse 1s forwards');
+        $('#T1').css('animation', 'collapse 0.5s forwards');
         setTimeout(function() {
             $('#T1').css('animation', 'recover 1s forwards');
         }, 2000);
